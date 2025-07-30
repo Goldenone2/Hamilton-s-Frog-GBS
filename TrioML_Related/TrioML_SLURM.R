d38bc38239ff3c80c9c81c61f027e78ee0d6243c
Hadley Muller <152338506+Goldenone2@users.noreply.github.com>
2025-07-30 12:16:06 +1200

    Update TrioML_SLURM.R

--- TrioML_Related/TrioML_SLURM.R
+++ TrioML_Related/TrioML_SLURM.R
@@ -9,12 +9,31 @@ library("vcfR")
 library("ggplot2")
 library("related")
 library("adegenet")
+library("dplyr")
 
 message("Packages_Loaded")
 
 # Run TrioML
-results <- coancestry("HamGeno.txt", trioml = 1, trioml.num.reference = 82)
+BoatBay_results <- coancestry("BoatBay.txt", trioml = 1, trioml.num.reference = 20)
 #RDS means we can save the R object for plotting later :) 
-saveRDS(results, file = "TrioML_results.rds")
+saveRDS(BoatBay_results, file = "BoatBay_results.rds")
+
+message("Boat Bay done")
+
+TePakeka_results <- coancestry("TePakeka.txt", trioml = 1, trioml.num.reference = 20)
+saveRDS(TePakeka_results, file = "TePakeka_results.rds")
+
+message("Te Pakeka done")
+
+Motuara_results <- coancestry("Motuara.txt", trioml = 1, trioml.num.reference = 27)
+saveRDS(Motuara_results, file = "Motuara_results.rds")
+
+message("Motuara done")
+
+Takapourewa_results <- coancestry("Takapourewa.txt", trioml = 1, trioml.num.reference = 15)
+saveRDS(Takapourewa_results, file = "Takapourewa_results.rds")
+
+message("Takapourewa done")
+
 
 warnings()
