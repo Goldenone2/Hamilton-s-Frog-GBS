5895f7b199b2ed280f3c98013de3e6f51f30a561
Hadley Muller <152338506+Goldenone2@users.noreply.github.com>
2025-08-14 16:01:32 +1200

    Update TrioML_SLURM.R

--- TrioML_Related/TrioML_SLURM.R
+++ TrioML_Related/TrioML_SLURM.R
@@ -15,23 +15,23 @@ message("Packages_Loaded")
 # these are great to check in the terminal my R code is working
 
 # Run TrioML
-BoatBay_results <- coancestry("BoatBay.txt", dyadml =1, wang =1 )
+BoatBay_results <- coancestry("BoatBay.txt", trioml = 1, trioml.num.reference = 20)
 #RDS means we can save the R object for plotting later :) 
 saveRDS(BoatBay_results, file = "BoatBay_results.rds")
 
 message("Boat Bay done")
 
-TePakeka_results <- coancestry("TePakeka.txt", dyadml =1, wang =1)
+TePakeka_results <- coancestry("TePakeka.txt", trioml = 1, trioml.num.reference = 20)
 saveRDS(TePakeka_results, file = "TePakeka_results.rds")
 
 message("Te Pakeka done")
 
-Motuara_results <- coancestry("Motuara.txt", dyadml =1, wang =1)
+Motuara_results <- coancestry("Motuara.txt", trioml = 1, trioml.num.reference = 27)
 saveRDS(Motuara_results, file = "Motuara_results.rds")
 
 message("Motuara done")
 
-Takapourewa_results <- coancestry("Takapourewa.txt", dyadml =1, wang =1)
+Takapourewa_results <- coancestry("Takapourewa_poly.txt", trioml = 1, trioml.num.reference = 15)
 saveRDS(Takapourewa_results, file = "Takapourewa_results.rds")
 
 message("Takapourewa done")
